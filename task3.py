6b0502c3356a7548052f658e2d128bb485925488
Oxygen <grandnorik@mail.ru>
2022-12-21 23:06:31 +0600

    Task3 done

--- task3.py
+++ task3.py
@@ -1,5 +1,19 @@
 # Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
 # максимум использование библиотеки Random для получения случайного int
 
+import random
+
+list = []
+
+for i in range(10):
+    list.append(i)
+
+print(list)
+
+for i in range(9, 0, -1):
+    j = random.randint(0, i + 1)
+    list[i], list[j] = list[j], list[i]
+    
+print(list)
 
 
